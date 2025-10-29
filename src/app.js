671dd1ee94308acc44d3b74489901cb90220fb5c
Anshika Bansal <anshikabansal1618@gmail.com>
2025-10-29 17:25:20 +0530

    added comments

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 // cookie-parser : used to set and access user's bowser cookies from server
 
 const app = express()
-
+ 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true
@@ -14,7 +14,7 @@ app.use(cors({
 // .use() used to config and setting middlewares
 app.use(express.json({limit: "16kb"}))                           
 app.use(express.urlencoded({extended: true, limit: "16kb"}))    // extended : object ke andar object
-app.use(express.static("public"))                               // public assets on our server
+app.use(express.static("public"))                               // public assets on our servers
 app.use(cookieParser())
 
 export { app }
