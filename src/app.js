7fc14ab29ffde046b702157a07df2d781cb18a49
Anshika Bansal <anshikabansal1618@gmail.com>
2025-10-29 17:13:11 +0530

    added comments

--- src/app.js
+++ src/app.js
@@ -11,9 +11,10 @@ app.use(cors({
   credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+// .use() used to config and setting middlewares
+app.use(express.json({limit: "16kb"}))                           
+app.use(express.urlencoded({extended: true, limit: "16kb"}))    // extended : object ke andar object
+app.use(express.static("public"))                               // public assets on our server
 app.use(cookieParser())
 
 export { app }
