fd0c21f0f2ea946977129a370feeb9f33340aea7
Anshika Bansal <anshikabansal1618@gmail.com>
2025-10-31 16:01:20 +0530

    fix all register controller bugs

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -2,9 +2,9 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({
-  cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME',
-  api_key: 'process.env.CLOUDINARY_API_KEY',
-  api_secret: 'process.env.CLOUDINARY_API_SECRET'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 // server to cloudinary - goal
@@ -23,7 +23,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     })
 
     // file has been uploaded successfully...response.url gives the public url after successful upload
-    console.log("file is uploaded on cloudinary", response.url);
+    //console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath) 
     return response;
 
   } catch (error) {
